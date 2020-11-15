94ca5a48f21c4764d35756d259805d37410ad4b7
Bruno <zettca@gmail.com>
2020-11-15 19:15:11 +0000

    add packages/ohmyzsh/git

---
+++ config.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# git
+git config --global user.name Bruno
+git config --global user.email zettca@gmail.com
+git config --global status.hints false
+git config --global core.editor vim
+
+# npm
+npm config set prefix ~/.local
+
---
+++ ohmyzsh/install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
+cp zettca.zsh-theme ${ZSH_CUSTOM}/themes/
+cp -r zettca/ ${ZSH_CUSTOM}/plugins/

f91ee8fe5bcae36937f77897a6cb7b78223d7ae3
Bruno <zettca@gmail.com>
2023-01-27 02:21:00 +0000

    refactor: use external plugin

--- ohmyzsh/install.sh
+++ ohmyzsh/install.sh
@@ -3,5 +3,5 @@
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zettca/zsh-zettca ${ZSH_CUSTOM}/plugins/zettca
 cp zettca.zsh-theme ${ZSH_CUSTOM}/themes/
-cp -r zettca/ ${ZSH_CUSTOM}/plugins/
